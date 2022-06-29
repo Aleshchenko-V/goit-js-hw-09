d976c3a1c268269b2db7be106c64f30f33003d3e
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-30 00:25:00 +0300

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.start.addEventListener('click', () => {
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    refs.body.style.backgroundColor = color;
+  }, 1000);
+});
+
+refs.stop.addEventListener('click', () => {
+  refs.stop.disabled = true;
+  refs.start.disabled = false;
+  clearInterval(timerId);
+});
